9b1d70cc45ea714cee893e0e26ae6e9c7cc2b3ba
VarshaGujrathi <varshagujrathi014@gmail.com>
2024-07-14 00:52:20 +0530

    New Commit

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
-import 'scroll.dart'; // Import the ScrollPage widget
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -17,9 +16,8 @@ class WelcomePage extends StatefulWidget {
 
 class _WelcomePageState extends State<WelcomePage> {
   final PageController _pageController = PageController();
-  int _currentPage = 0;
   late Timer _timer;
-  int _selectedIndex = 0; // Index of the currently selected bottom navigation item
+  int _selectedIndex = 0;
 
   final List<String> _images = [
     'assets/gofor.jpeg',
@@ -44,25 +42,46 @@ class _WelcomePageState extends State<WelcomePage> {
     'assets/fourty.jpg',
   ];
 
+  final List<String> _under199ImagesTopRow = [
+    'assets/footcat.jpg',
+    'assets/kurtacat.jpg',
+    'assets/kurticat.jpg',
+    'assets/lipcat.webp',
+    'assets/moistcat.jpg',
+    'assets/shirtcat.jpg',
+    'assets/shirtcatm.jpg',
+    'assets/shirtcatm.jpg',
+    'assets/tshirtcatw.jpg',
+    'assets/watchcat.jpg',
+  ];
+
+  final List<String> _under199ImagesBottomRow = [
+    'assets/bedcat.jpeg',
+    'assets/cascat.jpeg',
+    'assets/flipcat.jpeg',
+    'assets/jeanscat.jpeg',
+    'assets/kiddrcat.webp',
+    'assets/shampcat.jpeg',
+    'assets/shortcat.jpg',
+    'assets/sportcat.webp',
+    'assets/trackcat.avif',
+    'assets/trollycat.jpg',
+  ];
+
   @override
   void initState() {
     super.initState();
     _timer = Timer.periodic(Duration(seconds: 2), (Timer timer) {
-      if (_currentPage < _images.length - 1) {
-        setState(() {
-          _currentPage++;
-        });
-      } else {
-        setState(() {
-          _currentPage = 0;
-        });
+      if (_pageController.hasClients) {
+        if (_pageController.page == _images.length - 1) {
+          _pageController.jumpToPage(0);
+        } else {
+          _pageController.nextPage(
+            duration: Duration(milliseconds: 300),
+            curve: Curves.easeIn,
+          );
+        }
       }
-
-      _pageController.animateToPage(
-        _currentPage,
-        duration: Duration(milliseconds: 300),
-        curve: Curves.easeIn,
-      );
     });
   }
 
@@ -77,99 +96,74 @@ class _WelcomePageState extends State<WelcomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white, // Set app bar background color to white
+        backgroundColor: Colors.white,
         actions: [
           Padding(
-            padding: EdgeInsets.only(left: 20.0), // Add padding to adjust the position
-            child: _buildDropdownButton(context), // Custom dropdown button
+            padding: EdgeInsets.only(left: 20.0),
+            child: _buildDropdownButton(context),
           ),
           IconButton(
-            onPressed: () {
-              // Handle notification bell icon tap
-            },
-            icon: Icon(Icons.notifications, color: Colors.black), // Set icon color to black
+            onPressed: () {},
+            icon: Icon(Icons.notifications, color: Colors.black),
           ),
           IconButton(
-            onPressed: () {
-              // Handle wishlist icon tap
-            },
-            icon: Icon(Icons.favorite, color: Colors.black), // Set icon color to black
+            onPressed: () {},
+            icon: Icon(Icons.favorite, color: Colors.black),
           ),
           IconButton(
-            onPressed: () {
-              // Handle profile icon tap
-            },
-            icon: Icon(Icons.account_circle, color: Colors.black), // Set icon color to black
+            onPressed: () {},
+            icon: Icon(Icons.account_circle, color: Colors.black),
           ),
         ],
       ),
       body: SingleChildScrollView(
         child: Container(
-          color: Colors.white, // Set background color of the body to white
-          padding: EdgeInsets.all(16.0), // Add padding for content
+          color: Colors.white,
+          padding: EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 10.0), // Reduced space between app bar and search bar
+              SizedBox(height: 10.0),
 
-              // Search Bar with Icons
+              // Build your search bar widget
               _buildSearchBar(),
 
-              SizedBox(height: 20.0), // Add space below search bar
+              SizedBox(height: 20.0),
 
-              // Row of buttons
+              // Build your button row widget
               _buildButtonRow(),
 
-              SizedBox(height: 20.0), // Add space below button row
+              SizedBox(height: 20.0),
 
-              // Image Carousel
+              // Build your image carousel widget
               _buildImageCarousel(),
 
-              SizedBox(height: 10.0), // Add space below image carousel
+              SizedBox(height: 10.0),
 
-              // Text below carousel
-              Center(
-                child: Column(
-                  children: [
-                    Text(
-                      'END OF SEASON STEALS',
-                      style: TextStyle(
-                        fontSize: 22.0,
-                        fontWeight: FontWeight.w900,
-                        color: Colors.black,
-                      ),
-                    ),
-                    SizedBox(height: 1.0), // Add space between texts
-                    Text(
-                      'Hottest Offers, Right This Way!',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              // Build your additional images row widget
+              _buildAdditionalImagesRow(),
 
-              SizedBox(height: 10.0), // Space before additional images
-              _buildAdditionalImagesRow(), // Scrollable row of additional images
+              SizedBox(height: 20.0),
 
-              SizedBox(height: 20.0), // Space before row slider images
-              _buildRowSliderImages(), // Row slider of additional images
+              // Build your row slider images widget
+              _buildRowSliderImages(),
 
-              // Add other content of the page here
+              SizedBox(height: 20.0),
+
+              // Build your under 199 images widgets
+              _buildUnder199Images(),
             ],
           ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.white, // Set background color to white
+        backgroundColor: Colors.white,
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.grey,
         showSelectedLabels: true,
         showUnselectedLabels: true,
-        currentIndex: _selectedIndex, // Track the selected index
+        currentIndex: _selectedIndex,
         onTap: (index) {
           setState(() {
             _selectedIndex = index;
@@ -204,16 +198,13 @@ class _WelcomePageState extends State<WelcomePage> {
   Widget _buildDropdownButton(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => ScrollPage()),
-        );
+        // Handle dropdown button tap
       },
       child: Container(
         padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
         decoration: BoxDecoration(
-          color: Color.fromARGB(255, 238, 230, 219), // Set background color to white
-          border: Border.all(color: Color.fromARGB(255, 235, 204, 171), width: 2.0), // Yellow border
+          color: Color.fromARGB(255, 238, 230, 219),
+          border: Border.all(color: Color.fromARGB(255, 235, 204, 171), width: 2.0),
           borderRadius: BorderRadius.circular(8.0),
         ),
         child: Row(
@@ -223,8 +214,8 @@ class _WelcomePageState extends State<WelcomePage> {
               'Myntra',
               style: TextStyle(color: Colors.black, fontSize: 16.0),
             ),
-            SizedBox(width: 8.0), // Add space between text and icon
-            Icon(Icons.arrow_drop_down, color: Colors.black), // Dropdown icon
+            SizedBox(width: 8.0),
+            Icon(Icons.arrow_drop_down, color: Colors.black),
           ],
         ),
       ),
@@ -234,26 +225,26 @@ class _WelcomePageState extends State<WelcomePage> {
   Widget _buildSearchBar() {
     return Container(
       decoration: BoxDecoration(
-        color: Colors.grey[200], // Set background color for search bar
-        borderRadius: BorderRadius.circular(20.0), // Set border radius
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(20.0),
       ),
-      padding: EdgeInsets.symmetric(horizontal: 16.0), // Add padding inside search bar
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
       child: Row(
         children: [
-          Icon(Icons.search, color: Colors.grey[600]), // Magnifying glass icon
-          SizedBox(width: 10.0), // Add space between magnifying glass icon and text field
+          Icon(Icons.search, color: Colors.grey[600]),
+          SizedBox(width: 10.0),
           Expanded(
             child: TextField(
               decoration: InputDecoration(
                 hintText: 'Search',
-                border: InputBorder.none, // Remove border
+                border: InputBorder.none,
               ),
             ),
           ),
-          SizedBox(width: 10.0), // Add space between search field and icons
-          Icon(Icons.camera_alt), // Camera icon
-          SizedBox(width: 10.0), // Add space between icons
-          Icon(Icons.mic), // Speaker icon
+          SizedBox(width: 10.0),
+          Icon(Icons.camera_alt),
+          SizedBox(width: 10.0),
+          Icon(Icons.mic),
         ],
       ),
     );
@@ -261,25 +252,25 @@ class _WelcomePageState extends State<WelcomePage> {
 
   Widget _buildButtonRow() {
     return SingleChildScrollView(
-      scrollDirection: Axis.horizontal, // Enable horizontal scrolling
+      scrollDirection: Axis.horizontal,
       child: Row(
         children: [
           _buildSquareButton('Men', 'assets/men.webp'),
-          SizedBox(width: 10.0), // Space between buttons
+          SizedBox(width: 10.0),
           _buildSquareButton('Women', 'assets/women.jpg'),
-          SizedBox(width: 10.0), // Space between buttons
+          SizedBox(width: 10.0),
           _buildSquareButton('Kids', 'assets/both.webp'),
-          SizedBox(width: 10.0), // Space between buttons
+          SizedBox(width: 10.0),
           _buildSquareButton('Footwear', 'assets/heels.jpg'),
-          SizedBox(width: 10.0), // Space between buttons
+          SizedBox(width: 10.0),
           _buildSquareButton('Accessories', 'assets/purse.webp'),
-          SizedBox(width: 10.0), // Space between buttons
+          SizedBox(width: 10.0),
           _buildSquareButton('Watches', 'assets/watch.jpg'),
-          SizedBox(width: 10.0), // Space between buttons
+          SizedBox(width: 10.0),
           _buildSquareButton('Luggage', 'assets/trolly.jpg'),
           SizedBox(width: 10.0),
           _buildSquareButton('Gadgets', 'assets/headphone.webp'),
-          SizedBox(width: 10.0), // Space between buttons
+          SizedBox(width: 10.0),
           _buildSquareButton('Jewellery', 'assets/jewel.webp'),
         ],
       ),
@@ -302,7 +293,7 @@ class _WelcomePageState extends State<WelcomePage> {
               fit: BoxFit.cover,
             ),
           ),
-          SizedBox(height: 4.0), // Add space between image and label
+          SizedBox(height: 4.0),
           Text(label, style: TextStyle(fontSize: 12.0)),
         ],
       ),
@@ -311,7 +302,7 @@ class _WelcomePageState extends State<WelcomePage> {
 
   Widget _buildImageCarousel() {
     return Container(
-      height: 200.0, // Adjust height as needed
+      height: 200.0,
       child: PageView.builder(
         controller: _pageController,
         itemCount: _images.length,
@@ -329,17 +320,14 @@ class _WelcomePageState extends State<WelcomePage> {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
-        children: _additionalImages.map((imagePath) {
-          return Padding(
-            padding: EdgeInsets.symmetric(horizontal: 4.0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: Image.asset(
-                imagePath,
-                width: 160.0,
-                height: 200.0,
-                fit: BoxFit.cover,
-              ),
+        children: _additionalImages.map((image) {
+          return Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: Image.asset(
+              image,
+              width: 180.0,
+              height: 250.0,
+              fit: BoxFit.cover,
             ),
           );
         }).toList(),
@@ -348,24 +336,78 @@ class _WelcomePageState extends State<WelcomePage> {
   }
 
   Widget _buildRowSliderImages() {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: _rowSliderImages.map((imagePath) {
-          return Padding(
-            padding: EdgeInsets.symmetric(horizontal: 4.0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: Image.asset(
-                imagePath,
-                width: 80.0,
-                height: 80.0,
-                fit: BoxFit.cover,
-              ),
+    return Container(
+      height: 80.0,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: _rowSliderImages.length,
+        itemBuilder: (context, index) {
+          return Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: Image.asset(
+              _rowSliderImages[index],
+              width: 80.0,
+              height: 180.0,
+              fit: BoxFit.cover,
             ),
           );
-        }).toList(),
+        },
       ),
+    );
+  }
+
+  Widget _buildUnder199Images() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+    
+        SizedBox(height: 1.0),
+        Row(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: _under199ImagesTopRow.map((image) {
+                    return Container(
+                      margin: EdgeInsets.only(right: 10.0),
+                      child: Image.asset(
+                        image,
+                        width: 130.0,
+                        height: 160.0,
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 10.0),
+        Row(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: _under199ImagesBottomRow.map((image) {
+                    return Container(
+                      margin: EdgeInsets.only(right: 10.0),
+                      child: Image.asset(
+                        image,
+                        width: 130.0,
+                        height: 160.0,
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
